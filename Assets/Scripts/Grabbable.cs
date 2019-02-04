73712df514944587a879c348cfd3468b0638ce2d
Nicodemus Swayne <nichodemus@gmail.com>
2019-02-04 12:33:52 -0600

    pickup and throw object

--- Assets/Scripts/Grabbable.cs
+++ Assets/Scripts/Grabbable.cs
@@ -2,17 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+    [RequireComponent(typeof(Rigidbody))]
 public class Grabbable : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [HideInInspector]
+    public Hand _activeController = null;
 }
