11affc7ffb63946962af0fa491a9ef333cb32d16
Nicodemus Swayne <nichodemus@gmail.com>
2019-03-11 22:40:45 -0500

    Add Score and Ball Limit

--- Assets/Scripts/RespawnObjects.cs
+++ Assets/Scripts/RespawnObjects.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class RespawnObjects : MonoBehaviour
 {
     public Transform SphereSpawnPoint;
@@ -9,16 +10,57 @@ public class RespawnObjects : MonoBehaviour
     public GameObject SpherePrefab;
     public GameObject CubePrefab;
 
+    private int ballCount;
+    private int pinCount;
+    private TextMesh ballsRemaining;
+    private TextMesh score;
+    private TextMesh winText;
+    private TextMesh loseText;
+
+    private void Start()
+    {
+        pinCount = 0;
+        ballCount = 3;
+
+        ballsRemaining = (TextMesh)GameObject.Find("BallsRemaining").GetComponent<TextMesh>();
+        UpdateBallsRemaining();
+
+        score = (TextMesh)GameObject.Find("Score").GetComponent<TextMesh>();
+        UpdateScore();
+
+        winText = (TextMesh)GameObject.Find("WinText").GetComponent<TextMesh>();
+        winText.text = "";
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.isTrigger && other.gameObject.name == "Sphere")
         {
-            SpawnSphere(other);
+            ballCount = ballCount - 1;
+            UpdateBallsRemaining();
+            if (ballCount > 0)
+            {
+                SpawnSphere(other);
+            }
+            else if (pinCount != 6)
+            {
+                winText.text = "You Lose...";
+            }
+
         }
 
         if (other.isTrigger && other.gameObject.name == "Cube")
         {
             SpawnCube(other);
+        }
+        if (other.gameObject.CompareTag("Scorable"))
+        {
+            pinCount = pinCount + 1;
+            UpdateScore();
+            if (pinCount == 6)
+            {
+                winText.text = "You Win!!!";
+            }
         }
     }
 
@@ -38,5 +80,15 @@ public class RespawnObjects : MonoBehaviour
         GameObject clone;
         clone = Instantiate(CubePrefab, CubeSpawnPoint.position, CubeSpawnPoint.rotation);
         clone.gameObject.name = "Cube";
+    }
+
+    private void UpdateBallsRemaining()
+    {
+        ballsRemaining.text = "Balls Remaining: " + ballCount.ToString();
+    }
+
+    private void UpdateScore()
+    {
+        score.text = "Score: " + pinCount.ToString();
     }
 }
